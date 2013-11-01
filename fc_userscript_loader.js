6bcaf1f310014bbbb52145184bb120d29d664c77
Joel Bradshaw <cincodenada@gmail.com>
2013-10-31 19:45:10 -0700

    Updating loaders to point to my version

--- fc_userscript_loader.js
+++ fc_userscript_loader.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name           Frozen Cookies
 // @version        github-latest
-// @description    Userscript to load Frozen Cookies written by Icehawk78
+// @description    Userscript to load Frozen Cookies written by Icehawk78, modified by cincodenada
 // @author         shinji257
-// @homepage       https://github.com/Icehawk78/FrozenCookies
+// @homepage       https://github.com/cincodenada/FrozenCookies
 // @include        http://orteil.dashnet.org/cookieclicker/
-// @updateURL      http://icehawk78.github.io/FrozenCookies/fc_userscript_loader.js
-// @downloadURL    http://icehawk78.github.io/FrozenCookies/fc_userscript_loader.js
+// @updateURL      http://cincodenada.github.io/FrozenCookies/fc_userscript_loader.js
+// @downloadURL    http://cincodenada.github.io/FrozenCookies/fc_userscript_loader.js
 // ==/UserScript==
 
 function LoadFrozenCookies() {
   var js = document.createElement('script');
   js.setAttribute('type', 'text/javascript');
-  js.setAttribute('src', 'https://raw.github.com/Icehawk78/FrozenCookies/master/frozen_cookies.js');
+  js.setAttribute('src', 'https://raw.github.com/cincodenada/FrozenCookies/master/frozen_cookies.js');
   document.head.appendChild(js);
 }
 // It's not the best way but Chrome doesn't work with addEventListener... :(
